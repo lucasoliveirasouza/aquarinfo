19897bff5de5df16603868a097ce1517fc5c8899
lucassouzapb <lucas.souza@portobeloweb.com.br>
2022-04-11 13:00:53 -0300

    Organizar layout dos peixes #12

--- lib/view/peixe_agua_doce/peixe_agua_doce_detalhes.dart
+++ lib/view/peixe_agua_doce/peixe_agua_doce_detalhes.dart
@@ -2,6 +2,7 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:infoquario/models/peixe_agua_doce.dart';
 import 'package:infoquario/services/peixe_agua_doce_service.dart';
+import 'package:infoquario/widget/linha_tabela.dart';
 
 class PeixeAguaDoceDetalhesView extends StatefulWidget {
   PeixeAguaDoce peixe;
@@ -42,184 +43,125 @@ class _PeixeAguaDoceDetalhesViewState extends State<PeixeAguaDoceDetalhesView> {
                       width: 280,
                       height: 280,
                       child: Container(
-                        color: Colors.white,
+                        color: Colors.green.shade50,
                       ),
                     );
                   }
                 }),
-            Table(
-              children: [
-                TableRow(children: [
-                  Container(
-                    padding: EdgeInsets.only(right: 10),
-                    height: 23,
-                    child: Text(
-                      "Oi:",
-                      textAlign: TextAlign.right,
-                      style: TextStyle(
-                        fontSize: 17,
-                      ),
-                    ),
-                    color: Colors.green,
-                  ),
-                  Container(
-                    padding: EdgeInsets.only(left: 10),
-                    height: 23,
-                    child: Text(
-                      "Lucas",
-                      textAlign: TextAlign.left,
-                      style: TextStyle(fontSize: 17, color: Colors.green),
-                    ),
-                  )
-                ]),
-              ],
+            SizedBox(
+              height: 15,
+            ),
+            LinhaTabela(
+              valor: widget.peixe.nomeCientifico,
+              title: "Nome científico:",
             ),
             SizedBox(
-              height: 5,
+              height: 3,
             ),
-            Text(
-              "Nome popular: " + widget.peixe.nomePopular,
-              style: TextStyle(
-                fontSize: 17,
-              ),
+            LinhaTabela(
+              valor: widget.peixe.classe,
+              title: "Classe:",
             ),
             SizedBox(
-              height: 5,
+              height: 3,
             ),
-            Text(
-              "Nome científico: " + widget.peixe.nomeCientifico,
-              style: TextStyle(
-                fontSize: 17,
-              ),
+            LinhaTabela(
+              valor: widget.peixe.ordem,
+              title: "Ordem:",
             ),
             SizedBox(
-              height: 5,
+              height: 3,
             ),
-            Text(
-              "Classe: " + widget.peixe.classe,
-              style: TextStyle(
-                fontSize: 17,
-              ),
+            LinhaTabela(
+              valor: widget.peixe.familia,
+              title: "Familia:",
             ),
             SizedBox(
-              height: 5,
+              height: 3,
             ),
-            Text(
-              "Ordem: " + widget.peixe.ordem,
-              style: TextStyle(
-                fontSize: 17,
-              ),
+            LinhaTabela(
+              valor: widget.peixe.genero,
+              title: "Gênero:",
             ),
             SizedBox(
-              height: 5,
+              height: 3,
             ),
-            Text(
-              "Familia: " + widget.peixe.familia,
-              style: TextStyle(
-                fontSize: 17,
-              ),
+            LinhaTabela(
+              valor: widget.peixe.tipo,
+              title: "Tipo:",
             ),
             SizedBox(
-              height: 5,
+              height: 3,
             ),
-            Text(
-              "Gênero: " + widget.peixe.genero,
-              style: TextStyle(
-                fontSize: 17,
-              ),
+            LinhaTabela(
+              valor: widget.peixe.tamanho,
+              title: "Tamanho:",
             ),
             SizedBox(
-              height: 5,
+              height: 3,
             ),
-            Text(
-              "Tipo: " + widget.peixe.tipo,
-              style: TextStyle(
-                fontSize: 17,
-              ),
+            LinhaTabela(
+              valor: widget.peixe.tamanho,
+              title: "Expectativa de vida:",
             ),
             SizedBox(
-              height: 5,
+              height: 3,
             ),
-            Text(
-              "Tamanho: " + widget.peixe.tamanho,
-              style: TextStyle(
-                fontSize: 17,
-              ),
+            LinhaTabela(
+              valor: widget.peixe.populacaoMinima,
+              title: "População mínima:",
             ),
             SizedBox(
-              height: 5,
+              height: 3,
             ),
-            Text(
-              "Expectativa de vida: " + widget.peixe.expectativa,
-              style: TextStyle(
-                fontSize: 17,
-              ),
+            LinhaTabela(
+              valor: widget.peixe.tipoAquario,
+              title: "Tipo de aquário:",
             ),
             SizedBox(
-              height: 5,
+              height: 3,
             ),
-            Text(
-              "População mínima: " + widget.peixe.populacaoMinima,
-              style: TextStyle(
-                fontSize: 17,
-              ),
+            LinhaTabela(
+              valor: widget.peixe.volumeMinimo,
+              title: "Volume mínimo:",
             ),
             SizedBox(
-              height: 5,
+              height: 3,
             ),
-            Text(
-              "Tipo de aquário: " + widget.peixe.tipoAquario,
-              style: TextStyle(
-                fontSize: 17,
-              ),
+            LinhaTabela(
+              valor: widget.peixe.fachadaMinima,
+              title: "Fachada mínima:",
             ),
             SizedBox(
-              height: 5,
+              height: 3,
             ),
-            Text(
-              "Volume mínimo: " + widget.peixe.volumeMinimo,
-              style: TextStyle(
-                fontSize: 17,
-              ),
+            LinhaTabela(
+              valor: widget.peixe.fachadaMinima,
+              title: "Fachada mínima:",
             ),
             SizedBox(
-              height: 5,
+              height: 3,
             ),
-            Text(
-              "Fachada mínima: " + widget.peixe.fachadaMinima,
-              style: TextStyle(
-                fontSize: 17,
-              ),
+            LinhaTabela(
+              valor: widget.peixe.phAgua,
+              title: "pH da água:",
             ),
             SizedBox(
-              height: 5,
+              height: 3,
             ),
-            Text(
-              "pH da água: " + widget.peixe.phAgua,
-              style: TextStyle(
-                fontSize: 17,
-              ),
+            LinhaTabela(
+              valor: widget.peixe.temperatura,
+              title: "Temperatura da água:",
             ),
             SizedBox(
-              height: 5,
+              height: 3,
             ),
-            Text(
-              "Temperatura da água: " + widget.peixe.temperatura,
-              style: TextStyle(
-                fontSize: 17,
-              ),
+            LinhaTabela(
+              valor: widget.peixe.dificuldade,
+              title: "Dificuldade de criação:",
             ),
             SizedBox(
-              height: 5,
-            ),
-            Text(
-              "Dificuldade de criação: " + widget.peixe.dificuldade,
-              style: TextStyle(
-                fontSize: 17,
-              ),
-            ),
-            SizedBox(
-              height: 5,
+              height: 3,
             ),
           ],
         ),
---
+++ lib/widget/linha_tabela.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class LinhaTabela extends StatelessWidget {
+  String title;
+  String valor;
+
+  LinhaTabela({
+    Key? key,
+    required this.valor,
+    required this.title,
+  }) : super(key: key);
+
+  Widget build(BuildContext context) {
+    return Container(
+      child: Table(
+        children: [
+          TableRow(children: [
+            Container(
+              padding: EdgeInsets.only(right: 5),
+              child: Text(
+                title,
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 15,
+                ),
+              ),
+              color: Colors.green,
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 5),
+              child: Text(
+                valor,
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.green),
+              ),
+            )
+          ]),
+        ],
+      ),
+    );
+  }
+}
