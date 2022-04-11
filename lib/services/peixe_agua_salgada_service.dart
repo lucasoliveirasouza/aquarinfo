0041dae97afe801c6a5a85a3b3d334de0e8505ee
lucassouzapb <lucas.souza@portobeloweb.com.br>
2022-04-11 14:26:15 -0300

    Persitencia peixe de agua salgada #14

---
+++ lib/services/peixe_agua_salgada_service.dart
@@ -0,0 +1,85 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:infoquario/models/peixe_agua_salgada.dart';
+
+class PeixeAguaSalgadaService {
+  CollectionReference aguasalgada =
+      FirebaseFirestore.instance.collection('peixeaguasalgada');
+
+  PeixeAguaSalgadaService() {}
+
+  void registrarAguaSalgada(
+      nomePopular,
+      nomeCientifico,
+      classe,
+      ordem,
+      familia,
+      genero,
+      origem,
+      tipo,
+      tamanho,
+      expectativa,
+      populacaoMinima,
+      tipoAquario,
+      volumeMinimo,
+      fachadaMinima,
+      phAgua,
+      temperatura,
+      dificuldade,
+      imagem) {
+    aguasalgada.add({
+      'id': aguasalgada.id,
+      'nomePopular': nomePopular,
+      'nomeCientifico': nomeCientifico,
+      'classe': classe,
+      'ordem': ordem,
+      'familia': familia,
+      'genero': genero,
+      'origem': origem,
+      'tipo': tipo,
+      'tamanho': tamanho,
+      'expectativa': expectativa,
+      'populacaoMinima': populacaoMinima,
+      'tipoAquario': tipoAquario,
+      'volumeMinimo': volumeMinimo,
+      'fachadaMinima': fachadaMinima,
+      'phAgua': phAgua,
+      'temperatura': temperatura,
+      'dificuldade': dificuldade,
+      'imagem': imagem,
+    });
+  }
+
+  Future<List<PeixeAguaSalgada?>?> getAll(tipo) async {
+    List<PeixeAguaSalgada> posts = [];
+    QuerySnapshot snapshot =
+        await FirebaseFirestore.instance.collection('peixeaguasalgada').get();
+    snapshot.docs.forEach((d) {
+      PeixeAguaSalgada peixe = PeixeAguaSalgada(
+          d["id"],
+          d["nomePopular"],
+          d["nomeCientifico"],
+          d["classe"],
+          d["ordem"],
+          d["familia"],
+          d["genero"],
+          d["origem"],
+          d["tipo"],
+          d["tamanho"],
+          d["expectativa"],
+          d["populacaoMinima"],
+          d["tipoAquario"],
+          d["volumeMinimo"],
+          d["fachadaMinima"],
+          d["phAgua"],
+          d["temperatura"],
+          d["dificuldade"],
+          d["imagem"]);
+      if (d["tipo"] == tipo) {
+        posts.add(peixe);
+      } else if (tipo == "Todos") {
+        posts.add(peixe);
+      }
+    });
+    return posts;
+  }
+}
