0ace67d2535d9a4c4fdd449e17c63b85d64f3b00
lucassouzapb <lucas.souza@portobeloweb.com.br>
2022-04-13 07:10:05 -0300

    Persistência crustáceos #19

--- lib/view/peixe_agua_doce/peixe_agua_doce.dart
+++ lib/view/peixe_agua_doce/peixe_agua_doce.dart
@@ -1,9 +1,7 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:infoquario/view/peixe_agua_doce/peixe_agua_doce_cadastro.dart';
 import 'package:infoquario/view/peixe_agua_doce/peixe_agua_doce_lista.dart';
-
 
 import 'package:infoquario/widget/card_item.dart';
 
@@ -19,112 +17,110 @@ class _PeixeAguaDoceViewState extends State<PeixeAguaDoceView> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: GridView.count(
-          primary: false,
-          padding: const EdgeInsets.all(20),
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-          crossAxisCount: 2,
-          children: <Widget>[
-            CardItem(
-              image: "agua_doce/shoal.png",
-              title: "Todos",
-              color: Colors.green.shade100,
-              onTap: (){
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => PeixeAguaDoceListaView(tipo: "Todos"),
-                  ),
-                );
-              },
-            ),
-            CardItem(
-              image: "agua_doce/piranha.png",
-              title: "Carnívoros",
-              color: Colors.green.shade100,
-              onTap: (){
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => PeixeAguaDoceListaView(tipo: "Carnívoros"),
-                  ),
-                );
-              },
-            ),
-            CardItem(
-              image: "agua_doce/guppy.png",
-              title: "Herbívoros",
-              color: Colors.green.shade100,
-              onTap: (){
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => PeixeAguaDoceListaView(tipo: "Herbívoros"),
-                  ),
-                );
-              },
-            ),
-            CardItem(
-              image: "agua_doce/tetraneon.png",
-              title: "Onívoros",
-              color: Colors.green.shade100,
-              onTap: (){
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => PeixeAguaDoceListaView(tipo: "Onívoros"),
-                  ),
-                );
-              },
-            ),
-
-            CardItem(
-              image: "agua_doce/carpa.png",
-              title: "Planctógafos",
-              color: Colors.green.shade100,
-              onTap: (){
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => PeixeAguaDoceListaView(tipo: "Planctógafos"),
-                  ),
-                );
-              },
-            ),
-
-            CardItem(
-              image: "agua_doce/carpadetritos.png",
-              title: "Detritívoros",
-              color: Colors.green.shade100,
-              onTap: (){
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => PeixeAguaDoceListaView(tipo: "Detritívoros"),
-                  ),
-                );
-              },
-            ),
-
-          ],
-        )
-      ),
+          child: GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(20),
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        crossAxisCount: 2,
+        children: <Widget>[
+          CardItem(
+            image: "agua_doce/shoal.png",
+            title: "Todos",
+            color: Colors.green.shade100,
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => PeixeAguaDoceListaView(tipo: "Todos"),
+                ),
+              );
+            },
+          ),
+          CardItem(
+            image: "agua_doce/betta.png",
+            title: "Carnívoros",
+            color: Colors.green.shade100,
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      PeixeAguaDoceListaView(tipo: "Carnívoros"),
+                ),
+              );
+            },
+          ),
+          CardItem(
+            image: "agua_doce/guppy.png",
+            title: "Herbívoros",
+            color: Colors.green.shade100,
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      PeixeAguaDoceListaView(tipo: "Herbívoros"),
+                ),
+              );
+            },
+          ),
+          CardItem(
+            image: "agua_doce/tetraneon.png",
+            title: "Onívoros",
+            color: Colors.green.shade100,
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      PeixeAguaDoceListaView(tipo: "Onívoros"),
+                ),
+              );
+            },
+          ),
+          CardItem(
+            image: "agua_doce/carpa.png",
+            title: "Planctógafos",
+            color: Colors.green.shade100,
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      PeixeAguaDoceListaView(tipo: "Planctógafos"),
+                ),
+              );
+            },
+          ),
+          CardItem(
+            image: "agua_doce/carpadetritos.png",
+            title: "Detritívoros",
+            color: Colors.green.shade100,
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      PeixeAguaDoceListaView(tipo: "Detritívoros"),
+                ),
+              );
+            },
+          ),
+        ],
+      )),
       floatingActionButton: FloatingActionButton(
         child: Icon(
           Icons.add,
         ),
-        onPressed: (){
+        onPressed: () {
           Navigator.push(
             context,
             MaterialPageRoute(
-              builder: (context) => PeixeAguaDoceCadastroView()
-            ),
+                builder: (context) => PeixeAguaDoceCadastroView()),
           );
         },
       ),
     );
   }
 }
-
-
