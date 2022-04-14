ac64505ccd6e51c34946345bc10f1419df4e6e68
lucassouzapb <lucas.souza@portobeloweb.com.br>
2022-04-14 15:47:00 -0300

    Mudar tela de comentário #30

---
+++ lib/view/forum/comentario_cadastrar.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CometarioCadastrarView extends StatefulWidget {
+  const CometarioCadastrarView({Key? key}) : super(key: key);
+
+  @override
+  State<CometarioCadastrarView> createState() => _CometarioCadastrarViewState();
+}
+
+class _CometarioCadastrarViewState extends State<CometarioCadastrarView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(""),
+      ),
+    );
+  }
+}
