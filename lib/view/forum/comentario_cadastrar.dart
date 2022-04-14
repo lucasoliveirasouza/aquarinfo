937c7cc9f6bb4924b23cf3007f56d1c9b6ba0cb9
lucassouzapb <lucas.souza@portobeloweb.com.br>
2022-04-14 15:55:39 -0300

    Mudar tela de comentário #30

--- lib/view/forum/comentario_cadastrar.dart
+++ lib/view/forum/comentario_cadastrar.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:infoquario/services/comentario_service.dart';
 
 class CometarioCadastrarView extends StatefulWidget {
   const CometarioCadastrarView({Key? key}) : super(key: key);
@@ -8,11 +10,61 @@ class CometarioCadastrarView extends StatefulWidget {
 }
 
 class _CometarioCadastrarViewState extends State<CometarioCadastrarView> {
+  final FirebaseStorage storage = FirebaseStorage.instance;
+  final descricao = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(""),
+        title: Text("Responder"),
+      ),
+      body: Container(
+        child: ListView(
+          children: [
+            TextFormField(
+              minLines:
+                  5, // any number you need (It works as the rows for the textarea)
+              maxLines: null,
+              controller: descricao,
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                labelText: "Dúvidas/Sugestões/Dicas",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    new Radius.circular(10.0),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Container(
+              height: 50,
+              child: ElevatedButton(
+                child: Text("Cadastrar"),
+                onPressed: () {
+                  DateTime now = DateTime.now();
+
+                  String hora =
+                      "Às ${now.hour.toString().padLeft(2, '0')}:${now.minute.toString().padLeft(2, '0')} de ${now.day.toString().padLeft(2, '0')}/${now.month.toString().padLeft(2, '0')}/${now.year.toString()}";
+
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text(ComentarioService()
+                          .cadastrarComentario(
+                            descricao.text,
+                            widget.usuario,
+                            hora,
+                            widget.idForum,
+                          )
+                          .toString())));
+                  Navigator.of(context).pop();
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
